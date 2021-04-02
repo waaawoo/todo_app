af230c3defe7e455a5dd475a1c39dd31fadb396d
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-04-02 14:37:06 +0900

    一括削除実装

--- src/components/AllDel.js
+++ src/components/AllDel.js
@@ -1,11 +1,19 @@
-import React from 'react'
+// import React from 'react'
 
-class AllDel extends React.Component{
-  render(){
-    return (
-      <button>完了を全て削除する</button>
-    );
-  }
-}
+// class AllDel extends React.Component{
 
-export default AllDel;
+//   render(){
+//     return (
+//       // <button onClick={this.handleClickDeleteCompleted}>完了を全て削除する</button>
+//     );
+//   }
+
+//   // filter 配列にフィルターをかける
+//   // handleClickDeleteCompleted = () => {
+//   //   // 未完了のものだけnewTodosへ入る
+//   //   const newTodos = this.state.todos.fileter(({ completed }) => !completed )
+//   //   this.setState({ todos: newTodos })
+//   // }
+// }
+
+// export default AllDel;
